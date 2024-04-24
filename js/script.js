6ddfe6fbebc1a3384340c47101d9e0af2d354d23
elena_e <elenaka123@gmail.com>
2024-04-24 15:31:48 -0500

    Inital commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    //console.log(usersRequest);
+    const data = await usersRequest.json();
+    console.log(data);
+};
+
+getData();
+
+const userResults = data.results;
+console.log(userResults);
